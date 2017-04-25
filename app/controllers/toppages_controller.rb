cd390eb8a426a5240de5dfa20bb2f786f670708d
jiro.kirameki <taku.taku0617@gmail.com>
2017-04-25 07:35:07 +0000

    done

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -5,3 +5,10 @@ class ToppagesController < ApplicationController
     end
   end
 end
+class ToppagesController < ApplicationController
+  def index
+    if logged_in?
+      redirect_to controller: :users, action: :show, id: session[:user_id]
+    end
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show,]
+  before_action :require_user_logged_in, only: [:index, :show]
+  
   def index
     @users = User.all.page(params[:page])
   end
@@ -30,6 +31,14 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_path
+    end
+  end
+  
 end
 
 
